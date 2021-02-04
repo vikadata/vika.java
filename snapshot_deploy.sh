ba5ee3ee8684a08c637a70758c3160f56b70c39a
Shawn.Deng <dengguiheng@vikadata.com>
2021-02-04 09:09:41 +0800

    add gpg sign env variables

--- snapshot_deploy.sh
+++ snapshot_deploy.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 
 set -e
 
@@ -13,9 +14,14 @@ if [ -z "$SONATYPE_PASSWORD" ]; then
   exit 1
 fi
 
+if [ -z "$GPG_KEY_NAME" ]; then
+  echo "missing environment value: GPG_KEY_NAME" >&2
+  exit 1
+fi
+
 if [ -z "$GPG_PASSPHRASE" ]; then
   echo "missing environment value: GPG_PASSPHRASE" >&2
   exit 1
 fi
 
-mvn clean deploy sonatype-snapshot -DskipTests=true --settings .mvn/settings.xml
+mvn clean deploy -Psnapshot -DskipTests=true --settings "${TRAVIS_BUILD_DIR}/.mvn/settings.xml"
