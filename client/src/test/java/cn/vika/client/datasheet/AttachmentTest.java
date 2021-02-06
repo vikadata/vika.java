77d680c6e23f35c64d4ac52272450ce73056b65a
Shawn.Deng <dengguiheng@vikadata.com>
2021-02-06 17:13:58 +0800

    add record unit test

--- client/src/test/java/cn/vika/client/datasheet/AttachmentTest.java
+++ client/src/test/java/cn/vika/client/datasheet/AttachmentTest.java
@@ -1,13 +1,6 @@
 package cn.vika.client.datasheet;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import cn.vika.api.http.ApiCredential;
-import cn.vika.client.datasheet.model.AttachmentInfo;
-import cn.vika.client.datasheet.model.AttachmentRequest;
-import org.junit.jupiter.api.Assertions;
 
 /**
  * attachment test
@@ -18,12 +11,12 @@ import org.junit.jupiter.api.Assertions;
 public class AttachmentTest {
 
     public void testUploadAttachment() throws IOException {
-        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
-        AttachmentRequest param = new AttachmentRequest();
-        param.setFileName("维格数表通讯录导入模板.xlsx");
-        param.setFile(Files.readAllBytes(Paths.get("/**/**/维格数表通讯录导入模板.xlsx")));
-        AttachmentClient attachmentClient = new AttachmentClient(credential, "dst0Yj5aNeoHldqvf6");
-        AttachmentInfo attachmentInfo = attachmentClient.uploadAttachment(param);
-        Assertions.assertNotNull(attachmentInfo);
+//        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
+//        AttachmentRequest param = new AttachmentRequest();
+//        param.setFileName("维格数表通讯录导入模板.xlsx");
+//        param.setFile(Files.readAllBytes(Paths.get("/**/**/维格数表通讯录导入模板.xlsx")));
+//        AttachmentClient attachmentClient = new AttachmentClient(credential, "dst0Yj5aNeoHldqvf6");
+//        AttachmentInfo attachmentInfo = attachmentClient.uploadAttachment(param);
+//        Assertions.assertNotNull(attachmentInfo);
     }
 }
--- client/src/test/java/cn/vika/client/datasheet/RecordTest.java
+++ client/src/test/java/cn/vika/client/datasheet/RecordTest.java
@@ -1,16 +1,20 @@
 package cn.vika.client.datasheet;
 
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
-import cn.vika.api.http.ApiCredential;
-import cn.vika.client.datasheet.model.DeleteRecordRequest;
+import cn.vika.client.api.exception.ApiException;
+import cn.vika.client.api.http.ApiCredential;
+import cn.vika.client.api.model.ApiQueryParam;
+import cn.vika.client.api.model.Pager;
 import cn.vika.client.datasheet.model.Order;
-import cn.vika.client.datasheet.model.QueryRecordRequest;
-import cn.vika.client.datasheet.model.RecordInfo;
-import cn.vika.client.datasheet.model.RecordPageInfo;
-import cn.vika.client.datasheet.model.RecordRequest;
-import cn.vika.client.datasheet.model.SortRequest;
-import org.junit.jupiter.api.Assertions;
+import cn.vika.client.datasheet.model.RecordDetail;
+import cn.vika.core.utils.JacksonConverter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * test
@@ -19,101 +23,177 @@ import org.junit.jupiter.api.Assertions;
  * @date 2020-12-16 16:40:39
  */
 public class RecordTest {
-//    @Test
-    public void testQueryRecords() {
-        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
-        QueryRecordRequest param = new QueryRecordRequest();
-        RecordClient recordClient = new RecordClient(credential, "dst7urpLop4QdLAv3j");
-        RecordPageInfo pageInfo = recordClient.queryRecords(param);
-        Assertions.assertNotNull(pageInfo);
-        Assertions.assertEquals(pageInfo.getTotal(), pageInfo.getRecords().length);
+
+    private VikaApiClient vikaApiClient;
+
+    private String apiKey = System.getenv("API_KEY");
+
+    private String datasheetId = System.getenv("D_NAME");
+
+    @BeforeEach
+    public void setup() {
+        ApiCredential credential = new ApiCredential(apiKey);
+        if (vikaApiClient == null) {
+            vikaApiClient = new VikaApiClient(credential);
+        }
     }
 
-//    @Test
-    public void testQueryRecordsWithSort() {
-        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
-        QueryRecordRequest param = new QueryRecordRequest();
-        SortRequest[] sorts = new SortRequest[1];
-        SortRequest sort = new SortRequest();
-        sort.setField("数字ID");
-        sort.setOrder(Order.DESC);
-        sorts[0] = sort;
-        param.setSort(sorts);
-        RecordClient recordClient = new RecordClient(credential, "dst7urpLop4QdLAv3j");
-        RecordPageInfo pageInfo = recordClient.queryRecords(param);
-        Assertions.assertNotNull(pageInfo);
-        Assertions.assertTrue((int)pageInfo.getRecords()[0].getFields().get("数字ID") > (int)pageInfo.getRecords()[1]
-            .getFields().get("数字ID"));
+    @Test
+    public void testDefaultPage() throws ApiException {
+        Pager<RecordDetail> pager = vikaApiClient.getRecordApi().getRecords(datasheetId, 10);
+        assertThat(pager).isNotNull();
+
+        int itemNumber = 0;
+        int pageIndex = 0;
+        while (pager.hasNext()) {
+            List<RecordDetail> records = pager.next();
+            pageIndex++;
+            assertThat(pageIndex).isEqualTo(pager.getCurrentPage());
+            if (pageIndex < pager.getTotalPages()) {
+                assertThat(records.size()).isEqualTo(10);
+            }
+            for (RecordDetail record : records) {
+                itemNumber++;
+                System.out.format("page=%d, item=%d, recordId=%s \n", pageIndex, itemNumber, record.getRecordId());
+            }
+        }
     }
 
-//    @Test
-    public void testQueryAllRecords() {
-        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
-        QueryRecordRequest param = new QueryRecordRequest();
-        RecordClient recordClient = new RecordClient(credential, "dstUezl1Yh4wVNzgwl");
-        RecordInfo[] records = recordClient.queryAllRecords(param);
-        Assertions.assertNotNull(records);
-        Assertions.assertTrue(records.length > 1000);
+    @Test
+    public void testGetAllPage() throws ApiException {
+        Pager<RecordDetail> pager = vikaApiClient.getRecordApi().getRecords(datasheetId, 10);
+        assertThat(pager).isNotNull();
+        List<RecordDetail> records = pager.all();
+        int itemNumber = 0;
+        for (RecordDetail record : records) {
+            itemNumber++;
+            System.out.format("item=%d, recordId=%s \n", itemNumber, record.getRecordId());
+        }
     }
 
-//    @Test
+    @Test
+    public void testPageStream() throws ApiException {
+        Pager<RecordDetail> pager = vikaApiClient.getRecordApi().getRecords(datasheetId, 10);
+        assertThat(pager).isNotNull();
+        Stream<RecordDetail> records = pager.stream();
+        records.forEach(record -> System.out.format("recordId=%s \n", record.getRecordId()));
+    }
+
+    @Test
+    public void testPageByView() throws ApiException {
+        ApiQueryParam queryParam = ApiQueryParam.EMPTY.withView("viwPjaor1digt");
+        Pager<RecordDetail> pager = vikaApiClient.getRecordApi().getRecords(datasheetId, queryParam);
+        assertThat(pager).isNotNull();
+
+        List<RecordDetail> records = pager.all();
+
+        int itemNumber = 0;
+        for (RecordDetail record : records) {
+            itemNumber++;
+            System.out.format("item=%d, record=%s \n", itemNumber, JacksonConverter.toJson(record));
+        }
+    }
+
+    @Test
+    public void testPageBySort() throws ApiException {
+        ApiQueryParam queryParam = ApiQueryParam.EMPTY.withSort("语言", Order.DESC);
+        Pager<RecordDetail> pager = vikaApiClient.getRecordApi().getRecords(datasheetId, queryParam);
+        assertThat(pager).isNotNull();
+        List<RecordDetail> records = pager.all();
+        int itemNumber = 0;
+        for (RecordDetail record : records) {
+            itemNumber++;
+            System.out.format("item=%d, record=%s \n", itemNumber, JacksonConverter.toJson(record));
+        }
+    }
+
+    @Test
+    public void testPageWithFieldsFilter() throws ApiException {
+        ApiCredential credential = new ApiCredential("usk9Urb7NLr8b42SJCIH42J");
+        VikaApiClient vikaApiClient = new VikaApiClient(credential);
+        ApiQueryParam queryParam = ApiQueryParam.EMPTY.withFields(Arrays.asList("语言", "心裂"));
+        Pager<RecordDetail> pager = vikaApiClient.getRecordApi().getRecords("dstkbJ434jLJ40q4TQ", queryParam);
+        assertThat(pager).isNotNull();
+        List<RecordDetail> records = pager.all();
+        int itemNumber = 0;
+        for (RecordDetail record : records) {
+            itemNumber++;
+            System.out.format("item=%d, record=%s \n", itemNumber, JacksonConverter.toJson(record));
+        }
+    }
+
+    @Test
+    public void testPagesWithRecordIds() throws ApiException {
+        ApiQueryParam queryParam = ApiQueryParam.EMPTY.withRecordIds(Arrays.asList("recnUNvCc6YYp", "recX7OKsvrQyT"));
+        Pager<RecordDetail> pager = vikaApiClient.getRecordApi().getRecords(datasheetId, queryParam);
+        assertThat(pager).isNotNull();
+        List<RecordDetail> records = pager.all();
+        int itemNumber = 0;
+        for (RecordDetail record : records) {
+            itemNumber++;
+            System.out.format("item=%d, record=%s \n", itemNumber, JacksonConverter.toJson(record));
+        }
+    }
+
+
+    //    @Test
     public void testCreateRecords() {
-        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
-        RecordRequest param = new RecordRequest();
-        RecordInfo[] createRecords = new RecordInfo[2];
-        HashMap<String, Object> field1 = new HashMap<>();
-        field1.put("数字ID", 999);
-        field1.put("多行文本", "本质上和上面的需求是同一个999");
-        RecordInfo record1 = new RecordInfo();
-        record1.setFields(field1);
-        HashMap<String, Object> field2 = new HashMap<>();
-        RecordInfo record2 = new RecordInfo();
-        field2.put("数字ID", 1000);
-        field2.put("多行文本", "本质上和上面的需求是同一个1000");
-        record2.setFields(field2);
-        createRecords[0] = record1;
-        createRecords[1] = record2;
-        param.setRecords(createRecords);
-        RecordClient recordClient = new RecordClient(credential, "dst7urpLop4QdLAv3j");
-        RecordInfo[] records = recordClient.createRecords(param);
-        Assertions.assertNotNull(records);
-        Assertions.assertEquals(records.length, 2);
+//        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
+//        RecordRequest param = new RecordRequest();
+//        RecordInfo[] createRecords = new RecordInfo[2];
+//        HashMap<String, Object> field1 = new HashMap<>();
+//        field1.put("数字ID", 999);
+//        field1.put("多行文本", "本质上和上面的需求是同一个999");
+//        RecordInfo record1 = new RecordInfo();
+//        record1.setFields(field1);
+//        HashMap<String, Object> field2 = new HashMap<>();
+//        RecordInfo record2 = new RecordInfo();
+//        field2.put("数字ID", 1000);
+//        field2.put("多行文本", "本质上和上面的需求是同一个1000");
+//        record2.setFields(field2);
+//        createRecords[0] = record1;
+//        createRecords[1] = record2;
+//        param.setRecords(createRecords);
+//        RecordClient recordClient = new RecordClient(credential, "dst7urpLop4QdLAv3j");
+//        RecordInfo[] records = recordClient.createRecords(param);
+//        Assertions.assertNotNull(records);
+//        Assertions.assertEquals(records.length, 2);
     }
 
-//    @Test
+    //    @Test
     public void testModifyRecords() {
-        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
-        RecordRequest param = new RecordRequest();
-        RecordInfo[] createRecords = new RecordInfo[2];
-        HashMap<String, Object> field1 = new HashMap<>();
-        field1.put("数字ID", 6666);
-        field1.put("多行文本", "本质上和上面的需求是同一个6666");
-        RecordInfo record1 = new RecordInfo();
-        record1.setFields(field1);
-        record1.setRecordId("recWfiMuFk60P");
-        HashMap<String, Object> field2 = new HashMap<>();
-        RecordInfo record2 = new RecordInfo();
-        field2.put("数字ID", 88888);
-        field2.put("多行文本", "本质上和上面的需求是同一个88888");
-        record2.setFields(field2);
-        record2.setRecordId("recZmiZ5QyrTC");
-        createRecords[0] = record1;
-        createRecords[1] = record2;
-        param.setRecords(createRecords);
-        RecordClient recordClient = new RecordClient(credential, "dst7urpLop4QdLAv3j");
-        RecordInfo[] records = recordClient.modifyRecords(param);
-        Assertions.assertNotNull(records);
-        Assertions.assertEquals(records.length, 2);
+//        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
+//        RecordRequest param = new RecordRequest();
+//        RecordInfo[] createRecords = new RecordInfo[2];
+//        HashMap<String, Object> field1 = new HashMap<>();
+//        field1.put("数字ID", 6666);
+//        field1.put("多行文本", "本质上和上面的需求是同一个6666");
+//        RecordInfo record1 = new RecordInfo();
+//        record1.setFields(field1);
+//        record1.setRecordId("recWfiMuFk60P");
+//        HashMap<String, Object> field2 = new HashMap<>();
+//        RecordInfo record2 = new RecordInfo();
+//        field2.put("数字ID", 88888);
+//        field2.put("多行文本", "本质上和上面的需求是同一个88888");
+//        record2.setFields(field2);
+//        record2.setRecordId("recZmiZ5QyrTC");
+//        createRecords[0] = record1;
+//        createRecords[1] = record2;
+//        param.setRecords(createRecords);
+//        RecordClient recordClient = new RecordClient(credential, "dst7urpLop4QdLAv3j");
+//        RecordInfo[] records = recordClient.modifyRecords(param);
+//        Assertions.assertNotNull(records);
+//        Assertions.assertEquals(records.length, 2);
     }
 
-//    @Test
+    //    @Test
     public void testDeleteRecords() {
-        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
-        RecordClient recordClient = new RecordClient(credential, "dst7urpLop4QdLAv3j");
-        DeleteRecordRequest param = new DeleteRecordRequest();
-        param.setRecordIds(new String[] {"recWfiMuFk60P", "recZmiZ5QyrTC"});
-        boolean result = recordClient.deleteRecords(param);
-        Assertions.assertNotNull(result);
-        Assertions.assertTrue(result);
+//        ApiCredential credential = new ApiCredential(System.getenv("VIKA_TOKEN"));
+//        RecordClient recordClient = new RecordClient(credential, "dst7urpLop4QdLAv3j");
+//        DeleteRecordRequest param = new DeleteRecordRequest();
+//        param.setRecordIds(new String[] {"recWfiMuFk60P", "recZmiZ5QyrTC"});
+//        boolean result = recordClient.deleteRecords(param);
+//        Assertions.assertNotNull(result);
+//        Assertions.assertTrue(result);
     }
 }
